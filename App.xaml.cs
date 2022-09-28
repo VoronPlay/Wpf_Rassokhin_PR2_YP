fe4dbb338f76938e8a0cc4a7746595b275c8dd37
u31510 <u31510@P31510>
2022-09-29 09:42:18 +1000

    Добавьте файлы проекта.

---
+++ App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Wpf_Rassokhin_PR2_YP
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
